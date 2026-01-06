3f2e25b167486ae2122c91daa46e3115d856191b
Lucas Bibiana de Brito <lucas0brito@gmail.com>
2026-01-06 13:39:06 -0300

    Alterando a solução do exercício

--- aula_119/aula_119.py
+++ aula_119/aula_119.py
@@ -2,6 +2,8 @@
 # Crie funções que duplicam, triplicam e quadruplicam
 # o número recebido como parâmetro.
 
+# ---- MINHA SOLUÇÃO ----
+
 def multiply(num):
     def mod(multiplier):
         return num * multiplier
@@ -16,3 +18,19 @@ print(first_num(4))
 print(second_num(2))
 print(second_num(3))
 print(second_num(4))
+
+# ---- SOLUÇÃO DO PROFESSOR LUIZ OTÁVIO ----
+
+def criar_multiplicador(multiplicador):
+    def multiplicar(numero):
+        return numero * multiplicador
+    return multiplicar
+
+
+duplicar = criar_multiplicador(2)
+triplicar = criar_multiplicador(3)
+quadruplicar = criar_multiplicador(4)
+
+print(duplicar(2))
+print(triplicar(2))
+print(quadruplicar(2))
